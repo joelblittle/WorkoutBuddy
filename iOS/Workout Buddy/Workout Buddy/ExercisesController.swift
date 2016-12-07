d9e4088c5204e9d2c02ab4f9d3696bff4a045984
Rob Barber <rob@ethosstudios.co>
2016-12-07 10:39:32 -0800

    - Added exercise nav controller

---
+++ iOS/Workout Buddy/Workout Buddy/ExercisesController.swift
@@ -0,0 +1,14 @@
+//
+//  ExercisesController.swift
+//  Workout Buddy
+//
+//  Created by Robert Barber on 12/7/16.
+//  Copyright © 2016 Robert Barber. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesController {
+    
+    
+}
